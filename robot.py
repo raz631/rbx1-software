1d287747b356f497b0c6ac02b7ed79d3633e16b2
raz631 <34502262+raz631@users.noreply.github.com>
2017-12-17 17:28:01 +1100

    Update robot.py

--- robot.py
+++ robot.py
@@ -28,7 +28,7 @@ joints[0].setCurrent(65, 85, 75, 70)
 joints[1].setCurrent(65, 85, 85, 65)
 joints[2].setCurrent(50, 50, 50, 50)
 joints[3].setCurrent(60, 60, 60, 60)
-joints[4].setCurrent(60, 60, 60, 60)
+joints[4].setCurrent(50, 60, 60, 60)
 joints[5].setCurrent(60, 60, 60, 60)
 
 #setup the gripper
